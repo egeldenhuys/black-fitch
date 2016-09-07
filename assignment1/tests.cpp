6b2783e77af7f7f19f10a6a899c1df99fd34b41b
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2016-09-07 16:13:55 +0200

    Add test cases for task 2

--- assignment1/tests.cpp
+++ assignment1/tests.cpp
@@ -6,8 +6,321 @@
 
 #include "Wizard.h"
 #include "Spell.h"
+#include "Hobbit.h"
 
 using namespace std;
+
+SCENARIO( "wizard minus operator (-)")
+{
+    GIVEN( "a wizard with spells")
+    {
+        const int MAX_SPELLS = 25;
+
+        Wizard wiz;
+        wiz.setMaxNumberOfSpells(MAX_SPELLS);
+
+        Spell cloneSpells[10];
+        for (int i = 0; i < 10; i++)
+        {
+            cloneSpells[i] = Spell("CLONE", 12, 10);
+            wiz.addSpell(cloneSpells[i]);
+        }
+
+        Spell spells[5];
+        for (int i = 0; i < 5; i++)
+        {
+            spells[i] = Spell("SPELL_" + to_string(i), 12, 10);
+            wiz.addSpell(spells[i]);
+        }
+
+        WHEN( "a spell is removed with -")
+        {
+            wiz - spells[0].getName();
+
+            THEN( "all spells with same name is removed")
+            {
+                REQUIRE(wiz.getNumberOfSpells() == 14);
+
+                for (int i = 0; i < MAX_SPELLS; i++)
+                {
+                    REQUIRE(wiz.getSpell(i).getName() != spells[0].getName());
+                }
+
+            }
+        }
+
+        WHEN( "a duplicate spell is removed with -")
+        {
+            wiz - cloneSpells[0].getName();
+
+            THEN( "all spells with same name is removed")
+            {
+                REQUIRE(wiz.getNumberOfSpells() == 5);
+
+                for (int i = 0; i < MAX_SPELLS; i++)
+                {
+                    REQUIRE(wiz.getSpell(i).getName() != cloneSpells[0].getName());
+                }
+            }
+        }
+    }
+}
+
+SCENARIO( "wizard plus operator (+)")
+{
+    GIVEN( "a wizard")
+    {
+        Wizard wiz;
+
+        WHEN( "a spell is added using +")
+        {
+            Spell tmp("SPELL", 10, 5);
+
+            wiz + tmp;
+
+            THEN ("it is added to spell array")
+            {
+                REQUIRE(wiz.getSpell(0).getName() == "SPELL");
+                REQUIRE(wiz.getNumberOfSpells() == 1);
+            }
+
+            AND_WHEN("the spell name alread exists")
+            {
+                tmp.setSkillLevel(69);
+                wiz + tmp;
+
+                THEN( "The spell is set to the higher level")
+                {
+                    REQUIRE(wiz.getNumberOfSpells() == 1);
+                    REQUIRE(wiz.getSpell(0).getSkillLevel() == 69);
+                }
+            }
+        }
+
+    }
+}
+
+SCENARIO("wizard comparison operators (< >)")
+{
+    GIVEN("two wizards with same max size")
+    {
+        int MAX_SPELLS = 20;
+
+        Wizard wiz1;
+        wiz1.setMaxNumberOfSpells(MAX_SPELLS);
+
+        Wizard wiz2;
+        wiz2.setMaxNumberOfSpells(MAX_SPELLS);
+
+        WHEN("wizards have full spell array, and lhs has more higher skills than rhs")
+        {
+            Spell lhsSpells[MAX_SPELLS];
+            Spell rhsSpells[MAX_SPELLS];
+
+            for (int i = 0; i < MAX_SPELLS; i++) {
+                lhsSpells[i] = Spell("SPELL_" + to_string(i), 10, 10);
+                wiz1.addSpell(lhsSpells[i]);
+
+                rhsSpells[i] = Spell("SPELL_" + to_string(i), 10, 5);
+                wiz2.addSpell(rhsSpells[i]);
+            }
+
+            THEN( "lhs > rhs returns 1") {
+                REQUIRE( (wiz1 > wiz2) == 1);
+
+                AND_THEN ("rhs > lhs returns 0") {
+                    REQUIRE( (wiz2 > wiz1) == 0);
+                }
+            }
+
+            THEN( "lhs < rhs returns 0") {
+                REQUIRE( (wiz1 < wiz2) == 0);
+
+                AND_THEN ("rhs < lhs returns 1") {
+                    REQUIRE( (wiz2 < wiz1) == 1);
+                }
+            }
+        }
+
+        WHEN("wizards have full spell array, and rhs has more higher skills than lhs")
+        {
+            Spell lhsSpells[MAX_SPELLS];
+            Spell rhsSpells[MAX_SPELLS];
+
+            for (int i = 0; i < MAX_SPELLS; i++) {
+                lhsSpells[i] = Spell("SPELL_" + to_string(i), 10, 4);
+                wiz1.addSpell(lhsSpells[i]);
+
+                rhsSpells[i] = Spell("SPELL_" + to_string(i), 10, 5);
+                wiz2.addSpell(rhsSpells[i]);
+            }
+
+            THEN( "lhs > rhs returns 0") {
+                REQUIRE( (wiz1 > wiz2) == 0);
+
+                AND_THEN ("rhs > lhs returns 1") {
+                    REQUIRE( (wiz2 > wiz1) == 1);
+                }
+            }
+
+            THEN( "lhs < rhs returns 1") {
+                REQUIRE( (wiz1 < wiz2) == 1);
+
+                AND_THEN ("rhs < lhs returns 0") {
+                    REQUIRE( (wiz2 < wiz1) == 0);
+                }
+            }
+
+        }
+
+        WHEN( "lhs has more spells, and lhs has more higher skills" ) {
+            Spell lhsSpells[20];
+            Spell rhsSpells[10];
+
+            for (int i = 0; i < 20; i++) {
+                lhsSpells[i] = Spell("SPELL_" + to_string(i), 10, i + 1);
+                wiz1.addSpell(lhsSpells[i]);
+            }
+
+            for (int i = 0; i < 10; i++) {
+                rhsSpells[i] = Spell("SPELL_" + to_string(i), 10, i);
+                wiz2.addSpell(rhsSpells[i]);
+            }
+
+            THEN( "lhs > rhs returns 1") {
+                REQUIRE( (wiz1 > wiz2) == 1);
+
+                AND_THEN ("rhs > lhs returns 0") {
+                    REQUIRE( (wiz2 > wiz1) == 0);
+                }
+            }
+
+            THEN( "lhs < rhs returns 0") {
+                REQUIRE( (wiz1 < wiz2) == 0);
+
+                AND_THEN ("rhs < lhs returns 1") {
+                    REQUIRE( (wiz2 < wiz1) == 1);
+                }
+            }
+
+        }
+
+        WHEN( "lhs has less spells, and lhs has more higher skills" ) {
+            Spell lhsSpells[3];
+            Spell rhsSpells[15];
+
+            for (int i = 0; i < 3; i++) {
+                lhsSpells[i] = Spell("SPELL_" + to_string(i), 10, 11);
+                wiz1.addSpell(lhsSpells[i]);
+            }
+
+            for (int i = 0; i < 15; i++) {
+                rhsSpells[i] = Spell("SPELL_" + to_string(i), 10, 2);
+                wiz2.addSpell(rhsSpells[i]);
+            }
+
+            THEN( "lhs > rhs returns 1") {
+                REQUIRE( (wiz1 > wiz2) == 1);
+
+                AND_THEN ("rhs > lhs returns 0") {
+                    REQUIRE( (wiz2 > wiz1) == 0);
+                }
+            }
+
+            THEN( "lhs < rhs returns 0") {
+                REQUIRE( (wiz1 < wiz2) == 0);
+
+                AND_THEN ("rhs < lhs returns 1") {
+                    REQUIRE( (wiz2 < wiz1) == 1);
+                }
+            }
+        }
+
+        WHEN( "rhs has less spells, and lhs has more higher skills" ) {
+            Spell lhsSpells[15];
+            Spell rhsSpells[3];
+
+            for (int i = 0; i < 15; i++) {
+                lhsSpells[i] = Spell("SPELL_" + to_string(i), 10, 4);
+                wiz1.addSpell(lhsSpells[i]);
+            }
+
+            for (int i = 0; i < 3; i++) {
+                rhsSpells[i] = Spell("SPELL_" + to_string(i), 10, 3);
+                wiz2.addSpell(rhsSpells[i]);
+            }
+
+            THEN( "lhs > rhs returns 1") {
+                REQUIRE( (wiz1 > wiz2) == 1);
+
+                AND_THEN ("rhs > lhs returns 0") {
+                    REQUIRE( (wiz2 > wiz1) == 0);
+                }
+            }
+
+            THEN( "lhs < rhs returns 0") {
+                REQUIRE( (wiz1 < wiz2) == 0);
+
+                AND_THEN ("rhs < lhs returns 1") {
+                    REQUIRE( (wiz2 < wiz1) == 1);
+                }
+            }
+        }
+    }
+
+    GIVEN( "two wizards with different inventory sizes")
+    {
+        Wizard wiz1;
+        wiz1.setMaxNumberOfSpells(15);
+
+        Wizard wiz2;
+        wiz2.setMaxNumberOfSpells(30);
+
+        WHEN( "lhs has more higher skills than rhs")
+        {
+            Spell lhsSpells[15];
+            Spell rhsSpells[3];
+
+            for (int i = 0; i < 15; i++) {
+                lhsSpells[i] = Spell("SPELL_" + to_string(i), 10, 4);
+                wiz1.addSpell(lhsSpells[i]);
+            }
+
+            for (int i = 0; i < 3; i++) {
+                rhsSpells[i] = Spell("SPELL_" + to_string(i), 10, 3);
+                wiz2.addSpell(rhsSpells[i]);
+            }
+
+            THEN( "lhs > rhs returns 1" )
+            {
+                REQUIRE( (wiz1 > wiz2) == 1);
+
+                AND_THEN ("rhs > lhs returns 0") {
+                    REQUIRE( (wiz2 > wiz1) == 0);
+                }
+            }
+
+            THEN( "lhs < rhs returns 0") {
+                REQUIRE( (wiz1 < wiz2) == 0);
+
+                AND_THEN ("rhs > lhs returns 1") {
+                    REQUIRE( (wiz2 < wiz1) == 1);
+                }
+            }
+        }
+
+
+    }
+}
+
+TEST_CASE( "hobbit set and get functions" )
+{
+    Hobbit hob;
+
+    hob.setName("HOBBIT_NAME");
+    REQUIRE( hob.getName() == "HOBBIT_NAME" );
+
+}
 
 TEST_CASE( "spell map", "[!hide]")
 {
@@ -63,6 +376,38 @@ TEST_CASE( "wizard default constructor", "[wizard]")
             REQUIRE( tmpSpell.getName() == "" );
         }
     }
+
+}
+
+TEST_CASE( "wizard copy constructor")
+{
+    Wizard wiz1;
+    wiz1.setAge(69);
+    wiz1.setMaxNumberOfSpells(55);
+
+    int spellCount = 25;
+    Spell spells[spellCount];
+
+    for (int i = 0; i < spellCount; i++)
+    {
+        spells[i] = Spell("SPELL_" + to_string(i), i, i);
+
+        wiz1.addSpell(spells[i]);
+    }
+
+    Wizard wiz2(wiz1);
+
+    REQUIRE( wiz2.getAge() == 69 );
+    REQUIRE( wiz2.getMaxNumberOfSpells() == 55 );
+    REQUIRE( wiz2.getNumberOfSpells() == 25 );
+
+    for (int i = 0; i < 25; i++)
+    {
+        REQUIRE( wiz2.getSpell(i).getName() == spells[i].getName() );
+        REQUIRE( wiz2.getSpell(i).getDifficultyLevel() == spells[i].getDifficultyLevel() );
+        REQUIRE( wiz2.getSpell(i).getSkillLevel() == spells[i].getSkillLevel() );
+    }
+
 
 }
 /*
@@ -188,7 +533,7 @@ deleteSpell(string name): function to remove a Spell from the array of spells. R
 value of numberOfSpells and to set the values of the deleted value in the array to the values specified for the
 default constructor. In addition, remember to update numberOfLossedSpells.
 */
-TEST_CASE( "deleting spells from a wizard" , "[assume_delete_consolidates_array]")
+TEST_CASE( "deleting spells from a wizard" , "[assume_delete_does_not_consolidate]")
 {
     // Wizard
     Wizard wiz;
@@ -204,6 +549,12 @@ TEST_CASE( "deleting spells from a wizard" , "[assume_delete_consolidates_array]
         spells[i].setDifficultyLevel(i + 1); // odd
 
         wiz.addSpell(spells[i]);
+    }
+
+    SECTION( "when deleting a spell, the array is not consolidated") {
+        wiz.deleteSpell(spells[5].getName());
+        REQUIRE(wiz.getSpell(5).getName() == "");
+
     }
 
     SECTION( "when spell is deleted, the spell is no longer in wizard array" ) {
@@ -231,7 +582,6 @@ TEST_CASE( "deleting spells from a wizard" , "[assume_delete_consolidates_array]
         }
     }
 
-/*
     SECTION( "when adding a spell it is added to first empty name slot") {
         // Delete some spells in middle
         // 2, 4, 5, 8
@@ -297,7 +647,6 @@ TEST_CASE( "deleting spells from a wizard" , "[assume_delete_consolidates_array]
         REQUIRE( wiz.getNumberOfSpells() == spellCount - 1);
         REQUIRE( wiz.getNumberOfLossedSpells() == 1);
     }
-    */
 }
 
 TEST_CASE( "wizard setMaxNumberOfSpells()", "[assume_resize_only_called_at_init][!mayfail]")
