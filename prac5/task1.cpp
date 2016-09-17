9fbb550d821b8c3060c661e09e966f1e6d3c6a07
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2016-09-17 15:44:24 +0200

    Add tests for task 3

--- prac5/task1.cpp
+++ prac5/task1.cpp
@@ -2,22 +2,7 @@
 
 #include "Exception.h"
 
-
-/*
-Task 1: Exception Class [5]
-
-REQUIREMENTS
-============
-
-Class: Exception
-- A member variable storing the reason for the exception as a string.
-- A public function, what() , that returns the reason for the exception.
-- A public constructor that takes in a string describing the reason for the exception and
-initialises the member variable.
-
-*/
-
-TEST_CASE("exception constructor and what() function")
+TEST_CASE("exception constructor and what() function", "[task1]")
 {
     Exception ex("MY MESSAGE");
     REQUIRE(ex.what() == "MY MESSAGE");
