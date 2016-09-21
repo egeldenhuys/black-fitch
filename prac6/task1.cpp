82d0661cef1dc481883eaf7425f8f297ecc8f83e
Thomas <thomas@quantum-sicarius.za.net>
2016-09-21 20:49:18 +0200

    Fixed floating point comparison.

--- prac6/task1.cpp
+++ prac6/task1.cpp
@@ -2,6 +2,11 @@
 
 #include "cuboid.h"
 
+bool double_equals(double a, double b, double epsilon = 0.001)
+{
+        return std::abs(a - b) < epsilon;
+}
+
 TEST_CASE("testing cuboid<int> volume function", "[task1]")
 {
         Cuboid<int>* intc = new Cuboid<int>(2,4,5);
@@ -12,18 +17,22 @@ TEST_CASE("testing cuboid<int> surface area function", "[task1]")
 {
         Cuboid<int>* intc = new Cuboid<int>(2,4,5);
         REQUIRE(intc->getSurfaceArea() == 76);
+
 }
 
 
 TEST_CASE("testing cuboid<double> volume function", "[task1]")
 {
         Cuboid<double>* doublec = new Cuboid<double>(2.2,4.5,5.2);
-        REQUIRE(doublec->getVolume() == 51.48);
+        // Floating point errors
+        //REQUIRE(doublec->getVolume() == 51.48);
+        REQUIRE(double_equals(doublec->getVolume(),51.48));
 }
 
 
 TEST_CASE("testing cuboid<double> surface area function", "[task1]")
 {
         Cuboid<double>* doublec = new Cuboid<double>(2.2,4.5,5.2);
-        REQUIRE(doublec->getSurfaceArea() == 89.48);
+        REQUIRE(double_equals(doublec->getSurfaceArea(),89.48));
+
 }
