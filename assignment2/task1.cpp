1dddae0abe152d4dd4a363caa9c5ceb6a4829d7f
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2016-09-25 13:19:35 +0200

    Add tests for Task 1 - OTP

--- assignment2/task1.cpp
+++ assignment2/task1.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 
 #include "Caesar.h"
+#include "OneTimePad.h"
 
 using namespace std;
 
@@ -178,5 +179,51 @@ TEST_CASE("Caesar encoding/decoding", "[task1][caesar]")
     REQUIRE(encoded == expected);
     decoded = caes.decode(encoded);
     REQUIRE(decoded == newLongText);
+
+}
+
+TEST_CASE("oneTimePad encode/decode", "[task1][otp]")
+{
+
+    string newText = "Why, Hello.. There!";
+
+    string newLongText = string("Robert Lee Frost (March 26, 1874 - ") + \
+    "January 29, 1963) was an American poet. His work was initially published" + \
+    "in England before it was published in America. He is highly regarded for" + \
+    "his realistic depictions of rural life and his command of American" + \
+    "colloquial speech.[2] His work frequently employed settings from rural" + \
+    "life in New England in the early twentieth century, using them to examine" + \
+    "complex social and philosophical themes.";
+
+    string encoded;
+
+    OneTimePad pad(1);
+    encoded = pad.encode(newText);
+    REQUIRE(encoded == "Nm\\D23v@Y\\9@MA;z*K4");
+    REQUIRE(pad.decode(encoded) == newText);
+
+    pad.setSeed(2);
+    encoded = pad.encode(newText);
+
+    REQUIRE(encoded == "l%kVZ0frI$G+\"?Au2wS");
+    REQUIRE(pad.decode(encoded) == newText);
+
+    pad.setSeed(9999);
+    encoded = pad.encode(newText);
+
+    REQUIRE(encoded == "!x>Fz3la.!5%kwU\\Y8G");
+    REQUIRE(pad.decode(encoded) == newText);
+
+    encoded = pad.encode(newLongText);
+
+    string expected = string("{ ' m_'A'v'=^3`kfZsFe5%3C$X^=R{hc7mCp}c22)GWj") + \
+    "Z=c:xd8Nd#RuiG\"|+U2J[yy'1JECJ%cS5(}.L k>6Y<E-A,^)\\]9Bk;DBn>t*c^m(?-H5" + \
+    "&0`DZ=b?{r2aqP2t%6sECz\\S&XfR5PyK,\\A LF-q9viro?$v<&bIJ\\0jX>9BrNL;R7d=4(" + \
+    ";!3C<QaF$L!i!bqJqb!U@;%D0p`<G MbR*R$e$zm^L`-:gy<nD.Z3}MLMIyd{])e+4 52</3z0O" +
+    "R8kcSXP'<9ZZ<6cLll(L5NtPuU~NpK)c<4L)zM1c86ROen/kcJ_+F|Z=y\" |u;8B9uy!JUV4t\\." + \
+    "?F{8Flxh9}n,B5Lq+w8ktak/Ti(.Fyu6b~P8{VQn.4'(W`<m-@v,U,OGQ0wz>(L2s~mC>>@G+ZnVa1#y,;OetXXjsI3Oy,";
+
+    REQUIRE(encoded == expected);
+    REQUIRE(pad.decode(encoded) == newLongText);
 
 }
