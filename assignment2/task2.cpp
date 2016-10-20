6fc3b3627b72d6e123b2860c5bbff3a4cd133282
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2016-10-20 15:37:56 +0200

    fix

--- assignment2/task2.cpp
+++ assignment2/task2.cpp
@@ -1,3 +1,7 @@
+/*
+Input and expected output has been provided by the University of Pretoria
+*/
+
 #include "../catch/catch.hpp"
 
 #include <iostream>
@@ -9,17 +13,97 @@
 
 using namespace std;
 
-TEST_CASE("RowsColoumns eceptions")
+/*
+    It is unclear how fitchfork is testing this, if testing this at all.
+*/
+TEST_CASE("RowsColumns Exceptions", "[!mayfail]")
 {
+    string text="Hello, World!..";
+
+    string longText="Emily Elizabeth Dickinson was an American poet. Dickinson was born in Amherst, Massachusetts. Although part of a prominent family with strong ties to its community, Dickinson lived much of her life highly introverted.";
+    string encoded;
+
+    RowsColumns rc;
+
+    bool thrown;
+    string msg;
+    string decoded = "";
+
+    // As tested in given main.cpp
+    try {
+      decoded = rc.decode(text);
+    } catch (const char * e) {
+        thrown = true;
+        msg = e;
+    }
+
+    REQUIRE(thrown == true);
+    REQUIRE(msg == "Incompatible text length");
+    REQUIRE(decoded == "");
+
+    thrown = false;
+    msg = "";
+    decoded = "";
+
+    try {
+      decoded = rc.decode(longText);
+    } catch (const char * e) {
+      thrown = true;
+      msg = e;
+    }
+
+    REQUIRE(thrown == true);
+    REQUIRE(msg == "Incompatible text length");
+    REQUIRE(decoded == "");
+
+}
+
+/*
+    It is unclear how fitchfork is testing this, if testing this at all.
+*/
+TEST_CASE("ZigZag Exceptions", "[!mayfail]")
+{
+    string text="Hello, World!..";
+
+    string longText="Emily Elizabeth Dickinson was an American poet. Dickinson was born in Amherst, Massachusetts. Although part of a prominent family with strong ties to its community, Dickinson lived much of her life highly introverted.";
+    string encoded;
+
+    ZigZag zz;
+
+    bool thrown = false;
+    string msg = "";
+    string decoded = "";
+
+    try {
+      decoded = zz.decode(text);
+    } catch (const char * e) {
+      thrown = true;
+      msg = e;
+    }
+
+    REQUIRE(thrown == true);
+    REQUIRE(msg == "Incompatible text length");
+    REQUIRE(decoded == "");
+
+    thrown = false;
+    msg = "";
+    decoded = "";
+
+    try {
+      zz.decode(longText);
+    } catch (const char * e) {
+        thrown = true;
+        msg = e;
+    }
+
+    REQUIRE(thrown == true);
+    REQUIRE(msg == "Incompatible text length");
+    REQUIRE(decoded == "");
 
 }
 
 TEST_CASE("RowColumns encode/decode")
 {
-    /*
-    Input and expected output has been provided by the University of Pretoria
-
-    */
     string text="Hello, World!..";
 
     string longText="Emily Elizabeth Dickinson was an American poet. Dickinson was born in Amherst, Massachusetts. Although part of a prominent family with strong ties to its community, Dickinson lived much of her life highly introverted.";
@@ -37,43 +121,6 @@ TEST_CASE("RowColumns encode/decode")
     "bw nmuhmi tirn eap hs ittyv t tsoweepnho,elr h eartae   dio ");
     REQUIRE(rc.decode(encoded) == applyPadding(longText));
 
-    SECTION("RowColumns should throw exceptions when given length is not square")
-    {
-
-        bool thrown;
-        string msg;
-        string decoded = "";
-
-        // As tested in given main.cpp
-        try {
-          decoded = rc.decode(text);
-        } catch (const char * e) {
-            thrown = true;
-            msg = e;
-        }
-
-        INFO("Your code did not throw an exception!")
-        INFO("If you did throw an exception and printed it yourself it may also work.")
-        REQUIRE(thrown == true);
-        REQUIRE(msg == "Incompatible text length");
-        REQUIRE(decoded == "");
-
-        thrown = false;
-        msg = "";
-        decoded = "";
-
-        try {
-          decoded = rc.decode(longText);
-        } catch (const char * e) {
-          thrown = true;
-          msg = e;
-        }
-
-        REQUIRE(thrown == true);
-        REQUIRE(msg == "Incompatible text length");
-        REQUIRE(decoded == "");
-    }
-
 }
 
 TEST_CASE("ZigZag encode/decode")
@@ -83,7 +130,6 @@ TEST_CASE("ZigZag encode/decode")
     string longText="Emily Elizabeth Dickinson was an American poet. Dickinson was born in Amherst, Massachusetts. Although part of a prominent family with strong ties to its community, Dickinson lived much of her life highly introverted.";
     string encoded;
 
-
     ZigZag zz;
     encoded = zz.encode(text);
     REQUIRE(encoded == "Hoo!.r,el l. dWl");
@@ -92,35 +138,5 @@ TEST_CASE("ZigZag encode/decode")
     encoded = zz.encode(longText);
     REQUIRE(encoded == "E atsstrnsiD fveemittttst .nDmii  b,.  rscu rthck ofo  oDAclykmirMAfancihiedg nogm lancei Enrk stai msoh. lfomtl hsiiisliocnnaopyiun y   h ne ruc sanza noAhgowsilei  nrit imhumn wbeap hs ittyv t  rle,ohnpeewosth eartae   dio ");
     REQUIRE(zz.decode(encoded) == applyPadding(longText));
-
-    SECTION("ZigZag should throw exception when given length is not square")
-    {
-        bool thrown = false;
-        string msg = "";
-
-
-        try {
-          zz.decode(text);
-        } catch (const char * e) {
-          thrown = true;
-          msg = e;
-        }
-
-        REQUIRE(thrown == true);
-        REQUIRE(msg == "Incompatible text length");
-
-        thrown = false;
-        msg = "";
-
-        try {
-          zz.decode(longText);
-        } catch (const char * e) {
-            thrown = true;
-            msg = e;
-        }
-
-        REQUIRE(thrown == true);
-        REQUIRE(msg == "Incompatible text length");
-    }
 
 }
