76568473555f01fa5ce898f7f7cc95f46ef9a643
Thomas Scholtz <thomas@quantum-sicarius.za.net>
2016-10-25 17:29:31 +0200

    Added more tests.

--- assignment3/task1.cpp
+++ assignment3/task1.cpp
@@ -4,6 +4,8 @@
 #include <string>
 
 template <class T>
+
+/** This does not capture printf **/
 void captureDisplay(LinkedList<T> *ll, ostringstream &output) {
   // http://stackoverflow.com/questions/4810516/c-redirecting-stdout
 
@@ -42,7 +44,9 @@ TEST_CASE("testing linkedList<int> remove index 0", "[task1]") {
   ll->insert(0, 2);
   ll->insert(0, 3);
 
-  ll->remove(0);
+  int data = ll->remove(0);
+
+  REQUIRE(data == 3);
 
   ostringstream result;
   result << "[2,1]";
@@ -129,4 +133,163 @@ TEST_CASE("testing linkedList<int> remove invalid index from empty list",
   }
 
   REQUIRE(found == true);
+}
+
+TEST_CASE("testing linkedList<int> append two lists", "[task1]") {
+  LinkedList<int> *ll1 = new LinkedList<int>();
+
+  ll1->insert(0, 1);
+  ll1->insert(1, 2);
+  ll1->insert(2, 3);
+
+  LinkedList<int> *ll2 = new LinkedList<int>();
+
+  ll2->insert(0, 4);
+  ll2->insert(1, 5);
+  ll2->insert(2, 6);
+
+  LinkedList<int> ll3 = (*ll1 + *ll2);
+
+  ostringstream result;
+  result << "[1,2,3,4,5,6]";
+
+  ostringstream output;
+  captureDisplay(&ll3, output);
+
+  REQUIRE(output.str() == result.str());
+}
+
+TEST_CASE("testing linkedList<int> assignment operator", "[task1]") {
+  LinkedList<int> *ll1 = new LinkedList<int>();
+
+  ll1->insert(0, 1);
+  ll1->insert(1, 2);
+  ll1->insert(2, 3);
+
+  LinkedList<int> *ll2 = new LinkedList<int>();
+  ll2->insert(0, 9);
+
+  ll2 = ll1;
+
+  ostringstream result;
+  result << "[1,2,3]";
+
+  ostringstream output;
+  captureDisplay(ll2, output);
+
+  REQUIRE(output.str() == result.str());
+}
+
+TEST_CASE("testing linkedList<int> get leader", "[task1]") {
+  LinkedList<int> *ll1 = new LinkedList<int>();
+
+  ll1->insert(0, 1);
+  ll1->insert(1, 2);
+  ll1->insert(2, 3);
+
+  int data = ll1->getLeader()->data;
+
+  REQUIRE(data == 1);
+}
+
+TEST_CASE("testing linkedList<int> clear function", "[task1]") {
+  LinkedList<int> *ll1 = new LinkedList<int>();
+
+  ll1->insert(0, 1);
+  ll1->insert(1, 2);
+  ll1->insert(2, 3);
+
+  ll1->clear();
+
+  ostringstream result;
+  result << "[]";
+
+  ostringstream output;
+  captureDisplay(ll1, output);
+
+  REQUIRE(output.str() == result.str());
+}
+
+TEST_CASE("testing linkedList<int> [] operator", "[task1]") {
+  LinkedList<int> *ll1 = new LinkedList<int>();
+
+  ll1->insert(0, 1);
+  ll1->insert(1, 2);
+  ll1->insert(2, 3);
+
+  int data = (*ll1)[1];
+
+  REQUIRE(data == 2);
+}
+
+TEST_CASE("testing linkedList<int> inbetween insert", "[task1]") {
+  LinkedList<int> *ll1 = new LinkedList<int>();
+
+  ll1->insert(0, 1);
+  ll1->insert(1, 2);
+  ll1->insert(2, 3);
+
+  ostringstream result;
+  result << "[1,2,3]";
+
+  ostringstream output;
+  captureDisplay(ll1, output);
+
+  REQUIRE(output.str() == result.str());
+
+  ll1->insert(1, 99);
+
+  result.str("");
+  result << "[1,99,2,3]";
+
+  output.str("");
+  captureDisplay(ll1, output);
+
+  REQUIRE(output.str() == result.str());
+
+  ll1->insert(2, 88);
+
+  result.str("");
+  result << "[1,99,88,2,3]";
+
+  output.str("");
+  captureDisplay(ll1, output);
+
+  REQUIRE(output.str() == result.str());
+}
+
+TEST_CASE("testing linkedList<int> copy constructor", "[task1]") {
+  LinkedList<int> *ll1 = new LinkedList<int>();
+
+  ll1->insert(0, 1);
+  ll1->insert(1, 2);
+  ll1->insert(2, 3);
+
+  LinkedList<int> *ll2 = new LinkedList<int>(*ll1);
+
+  ostringstream result;
+  result << "[1,2,3]";
+
+  ostringstream output;
+  captureDisplay(ll2, output);
+
+  REQUIRE(output.str() == result.str());
+}
+
+TEST_CASE("testing linkedList<int> clone function", "[task1]") {
+  LinkedList<int> *ll1 = new LinkedList<int>();
+
+  ll1->insert(0, 1);
+  ll1->insert(1, 2);
+  ll1->insert(2, 3);
+
+  LinkedList<int> *ll2 = ll1->clone();
+
+  ostringstream result;
+  result << "[1,2,3]";
+
+  ostringstream output;
+  captureDisplay(ll2, output);
+
+  REQUIRE(output.str() == result.str());
 }
