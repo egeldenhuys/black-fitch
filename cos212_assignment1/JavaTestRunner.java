13526350a42dde3238eeb6df45fd780589d6db77
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2017-02-26 15:50:49 +0200

    Port tests for Stack to Java Fitch

--- cos212_assignment1/JavaTestRunner.java
+++ cos212_assignment1/JavaTestRunner.java
@@ -13,25 +13,40 @@ public class JavaTestRunner {
 	public static void main(String[] args) {
 
 		System.out.println("Starting Java Fitch...");
-		UnitTest suite_circularList = new tests_CircularList();
 
+		UnitTest suite_circularList = new tests_CircularList();
+		printHeader("CircularList");
 		result = result & suite_circularList.run();
 
-		// TODO: Feels bad
 		testsCount += suite_circularList.testsCount;
 		testsPassed += suite_circularList.testsPassed;
 		assertsCount += suite_circularList.assertsCount;
 		assertsPassed += suite_circularList.assertsPassed;
 
-		printFooter(result);
+		UnitTest suite_stack = new tests_Stack();
+		printHeader("Stack");
+		result = result & suite_stack.run();
+
+		testsCount += suite_stack.testsCount;
+		testsPassed += suite_stack.testsPassed;
+		assertsCount += suite_stack.assertsCount;
+		assertsPassed += suite_stack.assertsPassed;
+
+		printFooter("CircularList", result, suite_circularList.testsCount, suite_circularList.testsPassed, suite_circularList.assertsCount, suite_circularList.assertsPassed);
+		printFooter("Stack", result, suite_stack.testsCount, suite_stack.testsPassed, suite_stack.assertsCount, suite_stack.assertsPassed);
+		printFooter("OVERALL", result, testsCount, testsPassed, assertsCount, assertsPassed);
 	}
 
-	public static void printFooter(boolean result) {
-		System.out.println("======= SUMMARY =======");
+	public static void printHeader(String title) {
+		System.out.println("_________ SUITE: " + title + " _________");
+	}
+
+	public static void printFooter(String title, boolean result, int testsCount, int testsPassed, int assertsCount, int assertsPassed) {
+		System.out.println("________ SUMMARY for " + title + " _________");
 		System.out.println(resultToString(result));
 		System.out.println("tests: " + testsCount + " | " + testsPassed + " passed");
 		System.out.println("assertions: " + assertsCount + " | " + assertsPassed + " passed");
-		System.out.println("=======================");
+		System.out.println("_____________________________________________");
 	}
 
 	public static String resultToString(boolean value) {
