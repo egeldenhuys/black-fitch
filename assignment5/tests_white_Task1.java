a5e20dd9fbda525dfcfaae77e51fd25258f4ac55
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2017-05-26 21:17:55 +0200

    Add white-box test for clone()

--- assignment5/tests_white_Task1.java
+++ assignment5/tests_white_Task1.java
@@ -11,7 +11,8 @@ public class tests_white_Task1 extends JavaFitchUnitTest{
 	public boolean run() {
 
 		boolean result = true;
-		result = result & test_reconstructGraph("Reconstruct Graph");
+		result = result & test_reconstructGraph("reconstructGraph() and new Graph()");
+		result = result & test_clone("clone()");
 
 		return result;
 	}
@@ -52,6 +53,69 @@ public class tests_white_Task1 extends JavaFitchUnitTest{
 		result = end_test_case(result, subResult);
 		// TEST CASE END										^^^^
 		// =========================================================
+
+
+		// FOOTER
+		printFooter(title, result);
+		return result;
+		// FOOTER END
+	}
+
+	public boolean test_clone(String title) {
+		// HEADER
+		printHeader(title);
+		boolean result = true;
+		boolean subResult = true;
+		// HEADER END
+
+		// =========================================================
+		// TEST CASE START										VVVV
+		subResult = start_test_case("[g1] Constructing initial graph from graph.txt ( Graph() )");
+		// ==========================================================
+
+		Graph g1 = new Graph("graph.txt");
+
+		subResult = subResult & assertEquals("  A B C D \nA 0 2 1 4 \nB 2 0 7 3 \nC 1 7 0 5 \nD 4 3 5 0 \n", GraphUtils.matrixToString(g1.vertices, g1.matrix));
+
+		// =========================================================
+		result = end_test_case(result, subResult);
+		// TEST CASE END										^^^^
+		// =========================================================
+
+		// =========================================================
+		// TEST CASE START										VVVV
+		subResult = start_test_case("[g1] Reconstructing graph from g1.txt ( reconstructGraph() )");
+		// ==========================================================
+		boolean s = false;
+
+		s = g1.reconstructGraph("g1.txt");
+
+		subResult = subResult & assertEquals("  A Z B R E CAT \nA 0 5 3 0 0 2 \nZ 5 0 0 0 0 0 \nB 3 0 0 1 9 0 \nR 0 0 1 0 4 0 \nE 0 0 9 4 0 0 \nCAT 2 0 0 0 0 0 \n", GraphUtils.matrixToString(g1.vertices, g1.matrix));
+		subResult = subResult & assertEquals(true, s);
+		// =========================================================
+		result = end_test_case(result, subResult);
+		// TEST CASE END										^^^^
+		// =========================================================
+
+		// =========================================================
+		// TEST CASE START										VVVV
+		subResult = start_test_case("[g1] Cloning graph g1 and changing g1, then delete g1");
+		// ==========================================================
+
+		Graph g2 = g1.clone();
+		g1.reconstructGraph("graph.txt");
+
+		subResult = subResult & assertEquals("  A B C D \nA 0 2 1 4 \nB 2 0 7 3 \nC 1 7 0 5 \nD 4 3 5 0 \n", GraphUtils.matrixToString(g1.vertices, g1.matrix));
+		subResult = subResult & assertEquals("  A Z B R E CAT \nA 0 5 3 0 0 2 \nZ 5 0 0 0 0 0 \nB 3 0 0 1 9 0 \nR 0 0 1 0 4 0 \nE 0 0 9 4 0 0 \nCAT 2 0 0 0 0 0 \n", GraphUtils.matrixToString(g2.vertices, g2.matrix));
+
+		g1 = null;
+		subResult = subResult & assertEquals("  A Z B R E CAT \nA 0 5 3 0 0 2 \nZ 5 0 0 0 0 0 \nB 3 0 0 1 9 0 \nR 0 0 1 0 4 0 \nE 0 0 9 4 0 0 \nCAT 2 0 0 0 0 0 \n", GraphUtils.matrixToString(g2.vertices, g2.matrix));
+
+		// =========================================================
+		result = end_test_case(result, subResult);
+		// TEST CASE END										^^^^
+		// =========================================================
+
 
 
 		// FOOTER
