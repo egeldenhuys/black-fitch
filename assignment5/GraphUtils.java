1e1c9649985be79bedc2ed07631d2eea2256d81f
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2017-05-27 09:43:40 +0200

    Implement graphviz functions

--- assignment5/GraphUtils.java
+++ assignment5/GraphUtils.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Scanner;
+
 public class GraphUtils {
 
     // // TODO:
@@ -69,4 +74,81 @@ public class GraphUtils {
         return result;
 
     }
+
+    public static String getDotFromGraphFile(String fileName) {
+        String result = "";
+
+        result += "graph{splines=curved;sep=\"+25,+25\"; nodesep=\"1\";overlap=scalexy;";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            int lineCount = 1;
+
+            while ((line = br.readLine()) != null) {
+                if (lineCount == 1) {
+                    lineCount++;
+                } else {
+                    line = line.trim();
+                    line = line.replace(" ", "");
+                    line = line.replace("\n", "").replace("\r", "");
+
+                    if (!line.equals("")) {
+
+                        // Add to the array
+                        String[] elements = line.split(",");
+
+                        if (elements.length == 3) {
+                            result += elements[0] + "--" + elements[1] + "[ label= \"" + elements[2] + "\"];";
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+
+        }
+
+        result += "}";
+
+        return result;
+    }
+
+    public static void drawGraphFromFile(String inputFile, String outputFile) {
+        String dot = getDotFromGraphFile(inputFile);
+        drawGraphFromDot(dot, outputFile);
+    }
+
+    public static void drawGraphFromDot(String dot, String outputFile) {
+
+        JavaFitchLogger jLog = new JavaFitchLogger("graphviz", JavaFitchLogger.DEBUG);
+
+        ProcessBuilder builder = new ProcessBuilder("neato", "-T", "png", "-o", outputFile);
+        builder.redirectErrorStream(true);
+
+        try {
+
+            Process process = builder.start();
+
+            OutputStream stdin = process.getOutputStream(); // Our OUT is the process IN
+            InputStream stdout = process.getInputStream();
+
+            // stdin and stdout is now from thier perspective
+            BufferedReader reader = new BufferedReader(new InputStreamReader(stdout));
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
+
+            writer.write(dot);
+            writer.flush();
+            writer.close();
+
+            Scanner scanner = new Scanner(stdout);
+            while (scanner.hasNextLine()) {
+                jLog.log(JavaFitchLogger.ERROR, scanner.nextLine());
+            }
+
+        } catch (IOException e) {
+            jLog.log(JavaFitchLogger.ERROR, e.toString());
+            System.exit(1);
+        }
+
+    }
+
 }
